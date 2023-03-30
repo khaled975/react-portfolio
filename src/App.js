97d4e80a6614c6f872b51e795b5dbe6539776740
khaled975 <khaledbebo975@gmail.com>
2023-03-30 22:25:47 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Works from "./components/Works"
+import Contact from "./components/Contact"
+
 // import './App.css'
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Landing/>
-      <Skills/>
-      <Projects/>
-      <Footer/>
+      <BrowserRouter>
+        <Header/>
+        <Landing/>
+        <Skills/>
+        <Projects/>
+        <Routes>
+            <Route path="*" element={<Works/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/desc" element={<><p className='desc'>Contact With Me</p> <Contact/></>}/>
+        </Routes>
+        <Footer/>
+    </BrowserRouter>
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import {React} from "react";
 import {Col, Nav,Tab,Row} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
+import { Link , NavLink, Outlet} from "react-router-dom";
 import Contact from "./Contact"
 import Works from "./Works"
 import { BrowserRouter,Routes,Route } from "react-router-dom";
@@ -17,42 +18,16 @@ function Projects(){
                 <Tab.Container id="left-tabs-example" defaultActiveKey="first">
                     <Nav variant="pills" className="projects-tabs">
                         <Nav.Item>
-                            <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                            <Nav.Link><NavLink to="/">Tab 1</NavLink></Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                            <Nav.Link><Link to="contact">Tab 2</Link></Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                            <Nav.Link><Link to="desc">Tab 3</Link></Nav.Link>
                         </Nav.Item>
                     </Nav>
-                    <Tab.Content>
-                        <Tab.Pane eventKey="first" className="first-tab">
-                            <BrowserRouter>
-                                <Routes>
-                                    <Route path="/" element={<Works/>}/>
-                                </Routes>
-                            </BrowserRouter> 
-                       </Tab.Pane>
-                        <Tab.Pane eventKey="second" className="second-tab">
-                            <BrowserRouter>
-                                <Routes>
-                                    <Route path="/" element={<Contact/>}/>
-                                </Routes>
-                            </BrowserRouter>
-                        </Tab.Pane>
-                        <Tab.Pane eventKey="third">
-                            <BrowserRouter>
-                                <Routes>
-                                    <Route path="/" element={<p>Lorem</p>}/>
-                                </Routes>
-                                <Routes>
-                                    <Route path="/" element={<Contact/>}/>
-                                </Routes>
-                            </BrowserRouter>
-                                    hi
-                        </Tab.Pane>
-                    </Tab.Content>
+                    <Outlet/>
                 </Tab.Container>
         </section>
     )
